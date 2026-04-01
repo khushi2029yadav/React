8a21c608f398ff8cda87cc5049f5bbc0a3f898f7
khushiyadav1826 <khushi.yadav2029@vedamsot.org>
2026-04-02 05:08:02 +0530

    01-folder donet

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+// help krta hao coustom rules banane me jo ki har koi follow karega jaise koi string ko sigle qoutes me likhta hai koi double koi backticks to vo github pe merge hone me bahot isues hota hai to hum coustom rules banate hai isi folder me jo ki har koi follow kre
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+])
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
+
+//npm create vite -- to download the node_modules and react folder
